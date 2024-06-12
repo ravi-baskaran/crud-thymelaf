f7ee9f12c46c1483c65e386b5d958a529f6749be
Ravi Baskaran <ravibaskaran1611@gmail.com>
2024-06-12 08:13:25 +0530

    crud thymeleaf project

---
+++ src/main/java/net/javaguides/springboot/model/Employee.java
@@ -0,0 +1,74 @@
+package net.javaguides.springboot.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy =  GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "salary")
+	private int salary;
+	
+	@Column(name = "city")
+	private String city;
+	
+	@Column(name = "phoneNumber")
+	private String phoneNumber;
+	
+	
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
